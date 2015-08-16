17b62fbe4680b9b9e894019f54b8b34cc2619100
Fortyseven <fortyseven@gmail.com>
2015-08-16 12:39:24 -0400

    Initial import; connection controls

---
+++ BluetoothBarcodeReader/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace BluetoothBarcodeReader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose( bool disposing )
+        {
+            if ( disposing && ( components != null ) ) {
+                components.Dispose();
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupConnection = new System.Windows.Forms.GroupBox();
+            this.lblStopBits = new System.Windows.Forms.Label();
+            this.labelDataBits = new System.Windows.Forms.Label();
+            this.lblParity = new System.Windows.Forms.Label();
+            this.lblBaud = new System.Windows.Forms.Label();
+            this.lblPort = new System.Windows.Forms.Label();
+            this.cbPort = new System.Windows.Forms.ComboBox();
+            this.cbBaud = new System.Windows.Forms.ComboBox();
+            this.cbParity = new System.Windows.Forms.ComboBox();
+            this.cbStopBits = new System.Windows.Forms.ComboBox();
+            this.cbDataBits = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupConnection.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupConnection
+            // 
+            this.groupConnection.Controls.Add(this.cbStopBits);
+            this.groupConnection.Controls.Add(this.lblStopBits);
+            this.groupConnection.Controls.Add(this.cbDataBits);
+            this.groupConnection.Controls.Add(this.labelDataBits);
+            this.groupConnection.Controls.Add(this.cbParity);
+            this.groupConnection.Controls.Add(this.lblParity);
+            this.groupConnection.Controls.Add(this.cbBaud);
+            this.groupConnection.Controls.Add(this.lblBaud);
+            this.groupConnection.Controls.Add(this.cbPort);
+            this.groupConnection.Controls.Add(this.lblPort);
+            this.groupConnection.Location = new System.Drawing.Point(12, 12);
+            this.groupConnection.Name = "groupConnection";
+            this.groupConnection.Size = new System.Drawing.Size(590, 50);
+            this.groupConnection.TabIndex = 0;
+            this.groupConnection.TabStop = false;
+            this.groupConnection.Text = "Connection";
+            // 
+            // lblStopBits
+            // 
+            this.lblStopBits.AutoSize = true;
+            this.lblStopBits.Location = new System.Drawing.Point(487, 20);
+            this.lblStopBits.Name = "lblStopBits";
+            this.lblStopBits.Size = new System.Drawing.Size(49, 13);
+            this.lblStopBits.TabIndex = 8;
+            this.lblStopBits.Text = "Stop Bits";
+            // 
+            // labelDataBits
+            // 
+            this.labelDataBits.AutoSize = true;
+            this.labelDataBits.Location = new System.Drawing.Point(387, 20);
+            this.labelDataBits.Name = "labelDataBits";
+            this.labelDataBits.Size = new System.Drawing.Size(50, 13);
+            this.labelDataBits.TabIndex = 6;
+            this.labelDataBits.Text = "Data Bits";
+            // 
+            // lblParity
+            // 
+            this.lblParity.AutoSize = true;
+            this.lblParity.Location = new System.Drawing.Point(288, 20);
+            this.lblParity.Name = "lblParity";
+            this.lblParity.Size = new System.Drawing.Size(33, 13);
+            this.lblParity.TabIndex = 4;
+            this.lblParity.Text = "Parity";
+            // 
+            // lblBaud
+            // 
+            this.lblBaud.AutoSize = true;
+            this.lblBaud.Location = new System.Drawing.Point(187, 20);
+            this.lblBaud.Name = "lblBaud";
+            this.lblBaud.Size = new System.Drawing.Size(32, 13);
+            this.lblBaud.TabIndex = 2;
+            this.lblBaud.Text = "Baud";
+            // 
+            // lblPort
+            // 
+            this.lblPort.AutoSize = true;
+            this.lblPort.Location = new System.Drawing.Point(7, 20);
+            this.lblPort.Name = "lblPort";
+            this.lblPort.Size = new System.Drawing.Size(26, 13);
+            this.lblPort.TabIndex = 0;
+            this.lblPort.Text = "Port";
+            // 
+            // cbPort
+            // 
+            this.cbPort.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbPort.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cbPort.FormattingEnabled = true;
+            this.cbPort.Location = new System.Drawing.Point(39, 17);
+            this.cbPort.Name = "cbPort";
+            this.cbPort.Size = new System.Drawing.Size(141, 21);
+            this.cbPort.TabIndex = 1;
+            // 
+            // cbBaud
+            // 
+            this.cbBaud.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbBaud.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cbBaud.FormattingEnabled = true;
+            this.cbBaud.Items.AddRange(new object[] {
+            "300",
+            "600",
+            "1200",
+            "2400",
+            "4800",
+            "9600",
+            "14.4k",
+            "19.2k",
+            "38.4k",
+            "57.6k",
+            "115.2k"});
+            this.cbBaud.Location = new System.Drawing.Point(219, 17);
+            this.cbBaud.Name = "cbBaud";
+            this.cbBaud.Size = new System.Drawing.Size(63, 21);
+            this.cbBaud.TabIndex = 3;
+            // 
+            // cbParity
+            // 
+            this.cbParity.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbParity.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cbParity.FormattingEnabled = true;
+            this.cbParity.Items.AddRange(new object[] {
+            "None",
+            "Even",
+            "Odd",
+            "Mark",
+            "Space"});
+            this.cbParity.Location = new System.Drawing.Point(320, 17);
+            this.cbParity.Name = "cbParity";
+            this.cbParity.Size = new System.Drawing.Size(63, 21);
+            this.cbParity.TabIndex = 5;
+            // 
+            // cbStopBits
+            // 
+            this.cbStopBits.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbStopBits.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cbStopBits.FormattingEnabled = true;
+            this.cbStopBits.Items.AddRange(new object[] {
+            "1",
+            "1.5",
+            "2"});
+            this.cbStopBits.Location = new System.Drawing.Point(539, 14);
+            this.cbStopBits.Name = "cbStopBits";
+            this.cbStopBits.Size = new System.Drawing.Size(39, 21);
+            this.cbStopBits.TabIndex = 9;
+            // 
+            // cbDataBits
+            // 
+            this.cbDataBits.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbDataBits.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cbDataBits.FormattingEnabled = true;
+            this.cbDataBits.Items.AddRange(new object[] {
+            "5",
+            "6",
+            "7",
+            "8"});
+            this.cbDataBits.Location = new System.Drawing.Point(440, 17);
+            this.cbDataBits.Name = "cbDataBits";
+            this.cbDataBits.Size = new System.Drawing.Size(39, 21);
+            this.cbDataBits.TabIndex = 7;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(12, 69);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(590, 269);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Scan Data";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(614, 350);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupConnection);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "KDC200";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupConnection.ResumeLayout(false);
+            this.groupConnection.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupConnection;
+        private System.Windows.Forms.Label lblStopBits;
+        private System.Windows.Forms.Label labelDataBits;
+        private System.Windows.Forms.Label lblParity;
+        private System.Windows.Forms.Label lblBaud;
+        private System.Windows.Forms.Label lblPort;
+        private System.Windows.Forms.ComboBox cbStopBits;
+        private System.Windows.Forms.ComboBox cbDataBits;
+        private System.Windows.Forms.ComboBox cbParity;
+        private System.Windows.Forms.ComboBox cbBaud;
+        private System.Windows.Forms.ComboBox cbPort;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
