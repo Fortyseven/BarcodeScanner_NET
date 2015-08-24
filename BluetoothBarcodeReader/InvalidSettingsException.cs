e2f2916e72ebdd691bd155692eace9c2f4bdc5a3
Fortyseven <fortyseven@gmail.com>
2015-08-24 15:00:28 -0400

    Ported symbology list over from original code

---
+++ BluetoothBarcodeReader/InvalidSettingsException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BluetoothBarcodeReader
+{
+    class InvalidSettingsException : Exception
+    {
+        public InvalidSettingsException( string p )
+            : base( p )
+        {
+        }
+    }
+}
--- BluetoothBarcodeReader/Scanner/Symbology.cs
+++ BluetoothBarcodeReader/Scanner/Symbology.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace BluetoothBarcodeReader.Scanner
+﻿namespace BluetoothBarcodeReader.Scanner
 {
-    enum Symbology
+    public enum Symbology
     {
         UPC_E0,
         UPC_E1,
